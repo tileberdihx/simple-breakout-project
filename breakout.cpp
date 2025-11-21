dd074cd27c3b570e0c5877e5e5890b6ff8907837
Tileberdi Nurmatov <nt14018@auca.kg>
2025-11-21 20:50:59 +0600

    add - 3rd level, start menu, pause menu, victory menu

--- breakout.cpp
+++ breakout.cpp
@@ -10,34 +10,74 @@
 void update()
 {
     // TODO
+    switch (game_state) {
+    case menu_state:
+        if (IsKeyPressed(KEY_ENTER)) {
+            game_state = in_game_state;
+        }
+        break;
+    case in_game_state:
+        if (IsKeyPressed(KEY_ESCAPE)) {
+            game_state = paused_state;
+        }
+        if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
+            move_paddle(-paddle_speed);
+        }
+        if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
+            move_paddle(paddle_speed);
+        }
+        move_ball();
+        if (!is_ball_inside_level()) {
+            load_level();
+            PlaySound(lose_sound);
+        } else if (current_level_blocks == 0) {
+            load_level(1);
+            PlaySound(win_sound);
+        } else if (current_level_index == level_count) {
+            game_state = victory_state;
+        }
+        break;
+    case paused_state:
+        if (IsKeyPressed(KEY_ESCAPE)) {
+            game_state = in_game_state;
+        }
+        break;
+    case victory_state:
+        if (IsKeyPressed(KEY_ENTER)) {
+            game_state = in_game_state;
+        }
+        break;
 
-    if (IsKeyPressed(KEY_ESCAPE)) {
-        game_state = paused_state;
+    default:
+
+
     }
-    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
-        move_paddle(-paddle_speed);
-    }
-    if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
-        move_paddle(paddle_speed);
-    }
-    move_ball();
-    if (!is_ball_inside_level()) {
-        load_level();
-        PlaySound(lose_sound);
-    } else if (current_level_blocks == 0) {
-        load_level(1);
-        PlaySound(win_sound);
-    }
+
+
 }
 
 void draw()
 {
     // TODO
+    switch (game_state) {
+    case menu_state:
+        draw_menu();
+        break;
+    case in_game_state:
+        draw_level();
+        draw_paddle();
+        draw_ball();
+        draw_ui();
+        break;
+    case paused_state:
+        draw_pause_menu();
+        break;
+    case victory_state:
+        draw_victory_menu();
+        break;
+    default:
+    }
 
-    draw_level();
-    draw_paddle();
-    draw_ball();
-    draw_ui();
 }
 
 int main()
